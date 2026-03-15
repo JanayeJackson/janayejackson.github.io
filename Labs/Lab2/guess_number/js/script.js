eab204fae70634a06e8cca2eca8ea3b8491959f6
JanayeJackson <janayes.jackson@outlook.com>
2026-03-15 16:58:40 -0700

    Updating Website & working on Lab 2

---
+++ Labs/Lab2/guess_number/js/script.js
@@ -0,0 +1,79 @@
+//Event Listeners
+document.querySelector("#guessBtn").addEventListener("click", checkGuess);
+document.querySelector("#resetBtn").addEventListener("click", initializeGame);
+
+//Global variables
+let randomNumber;
+let attempts = 0;
+let wins = 0;
+let losses = 0;
+
+initializeGame();
+
+function initializeGame() {
+   randomNumber = Math.floor(Math.random() * 99) + 1;
+   console.log("randomNumber: " + randomNumber);
+   attemps = 0;
+
+   //Adding wins and losses stats
+   document.querySelector("#wins").textContent = wins;
+   document.querySelector("#losses").textContent = losses;
+
+   //hiding the Reset button
+   document.querySelector("#resetBtn").style.display = "none";
+  
+   //showing the Guess button
+   document.querySelector("#guessBtn").style.display = "inline";
+
+   let playerGuess = document.querySelector("#playerGuess");
+   playerGuess.focus(); //adding focus to textbox
+   playerGuess.value = ""; //clearing the textbox
+
+   let feedback = document.querySelector("#feedback");
+   feedback.textContent = ""; //clearing the textbox
+
+   document.querySelector("#guesses").textContent = "";
+}
+
+function checkGuess() {
+    let feedback = document.querySelector("#feedback");
+    feedback.textContent = "";
+    let guess = document.querySelector("#playerGuess").value;
+    console.log("Player guess: " + guess);
+    if(guess < 1 || guess > 99){
+        let feedback = document.querySelector("#feedback");
+        feedback.textContent = "Enter a number between 1 and 99";
+        feedback.style.color = "red";
+        return;
+    }
+
+    attempts++;
+
+    console.log("Attemps:" + attempts);
+    feedback.style.color = "blue";
+    if(guess == randomNumber){
+        feedback.textContent = "You guessed it! You Won!";
+        feedback.style.color = "darkblue";
+        wins++;
+    } else {
+        document.querySelector("#guesses").textContent += guess + " ";
+        if(attempts == 7){
+            feedback.textContent = "Sorry, you do not have any more guesses!";
+            feedback.style.color = "red";
+            losses++;
+            gameOver();
+        } else if (guess > randomNumber) {
+            feedback.textContent = "Guess was to high";
+        } else {
+            feedback.textContent = "Guess was to low";
+        }
+
+    }
+}
+
+function gameOver(){
+    let guessBtn = document.querySelector("#guessBtn");
+    let resetBtn = document.querySelector("#resetBtn");
+    guessBtn.style.display = "none";
+    resetBtn.style.display = "inline";
+}
